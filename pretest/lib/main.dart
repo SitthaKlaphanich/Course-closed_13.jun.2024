2b45a5a317ad65f726f4e2c42dda0aee7bad17fe
DragonDarc <pokemonmega1@gmail.com>
2024-06-13 09:14:10 +0700

    Huhhhhhhhhhhhhhhhhhhhhhhhhhh

--- pretest/lib/main.dart
+++ pretest/lib/main.dart
@@ -1,58 +1,71 @@
-import 'package:flutter/material.dart';
+// -------------------------1 Title : Center widget------------------------------------
 
-void main() {
-  runApp(MyApp());
-}
+// import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Stateful Widget Example',
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(backgroundColor: Colors.blue),
-      ),
-      home: InputForm(),
-    );
-  }
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
-class InputForm extends StatefulWidget {
-  @override
-  _InputFormState createState() => _InputFormState();
-}
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-class _InputFormState extends State<InputForm> {
-  String _inputText = '';
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Hello ครับ'),
+//         ),
+//         body: Center(
+//           // heightFactor: 1,
+//           // widthFactor: 1,
+//           child: Container(color: Colors.amber, child: Text('Hello')),
+//         ),
+//       ),
+//       theme: ThemeData(
+//         appBarTheme: const AppBarTheme(backgroundColor: Colors.blue),
+//       ),
+//     );
+//   }
+// }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Hello ครับ'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'กรุณาป้อนข้อความ:',
-            ),
-            TextField(
-              onChanged: (value) {
-                setState(() {
-                  _inputText = value;
-                });
-              },
-            ),
-            SizedBox(height: 20),
-            Text(
-              'ข้อความที่คุณป้อน: $_inputText',
-              style: TextStyle(fontSize: 18),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// -------------------------2 Title : Column widget------------------------------------
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text("Hello ครับ"),
+//         ),
+//         body: Container(
+//           color: Colors.amber,
+//           child: const Column(
+//             mainAxisAlignment: MainAxisAlignment.start,
+//             // crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Icon(Icons.book, size: 48),
+//               Icon(Icons.book, size: 48),
+//               Icon(Icons.book, size: 48),
+//             ],
+//           ),
+//         ),
+//       ),
+//       theme: ThemeData(
+//         appBarTheme: const AppBarTheme(backgroundColor: Colors.blue),
+//       ),
+//     );
+//   }
+// }
+
+// -------------------------3 Title : Stateful widget------------------------------------
+
