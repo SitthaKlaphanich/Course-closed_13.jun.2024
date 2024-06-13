7ecadad25f3926a7909dec660dda7681a8fac1e9
DragonDarc <pokemonmega1@gmail.com>
2024-06-13 09:36:23 +0700

    finish

--- pretest/lib/main.dart
+++ pretest/lib/main.dart
@@ -69,3 +69,119 @@
 
 // -------------------------3 Title : Stateful widget------------------------------------
 
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   int counter = 0;
+
+//   void addCounter() {
+//     setState(() {
+//       counter++;
+//     });
+//   }
+
+//   void delCounter() {
+//     setState(() {
+//       counter--;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Hello ครับ'),
+//         ),
+//         body: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: <Widget>[
+//               Text(
+//                 '$counter',
+//                 style: TextStyle(fontSize: 24),
+//               ),
+//               SizedBox(height: 20),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: <Widget>[
+//                   ElevatedButton(
+//                     onPressed: addCounter,
+//                     child: Icon(Icons.add),
+//                   ),
+//                   SizedBox(width: 20),
+//                   ElevatedButton(
+//                     onPressed: delCounter,
+//                     child: Icon(Icons.remove),
+//                   ),
+//                 ],
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//       theme: ThemeData(
+//         appBarTheme: const AppBarTheme(backgroundColor: Colors.blue),
+//       ),
+//     );
+//   }
+// }
+
+// -------------------------3 Title : Stateless widget------------------------------------
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Stateless Widget Example',
+//       home: CounterDisplay(),
+//       theme: ThemeData(
+//         appBarTheme: const AppBarTheme(backgroundColor: Colors.blue),
+//       ),
+//     );
+//   }
+// }
+
+// class CounterDisplay extends StatelessWidget {
+//   final int counter = 0;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Hello ครับ'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text(
+//               'ค่าของ counter:',
+//             ),
+//             Text(
+//               '$counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
